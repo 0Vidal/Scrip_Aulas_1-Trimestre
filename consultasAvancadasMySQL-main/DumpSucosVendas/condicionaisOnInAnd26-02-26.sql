156615b4255210ae759527772c664b8fd0929034
0Vidal <oliveira.vidal.vinicius@escola.pr.gov.br>
2026-02-26 11:46:25 -0300

    Add files via upload

---
+++ consultasAvancadasMySQL-main/DumpSucosVendas/condicionaisOnInAnd26-02-26.sql
@@ -0,0 +1,15 @@
+use sucos_vendas;
+
+
+select * from tabela_de_produtos
+where sabor = 'Manga' and not (tamanho = '470 ml');
+
+select * from tabela_de_produtos
+where sabor = 'Manga' or not (tamanho = '470 ml');
+
+select * from tabela_de_produtos where
+sabor in ('Manga', 'Laranja');
+
+select * from tabela_de_clientes 
+where cidade in ('Rio de Janeiro', 'São Paulo') 
+and idade >= 20;
